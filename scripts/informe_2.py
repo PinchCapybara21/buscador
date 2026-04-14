8d0ce39c48158d67a3d59e21341ad4107ddafe16
PinchCapybara21 <juanperillas@usantotomas.edu.co>
2026-04-14 14:24:11 -0500

    Update informe_2.py

--- scripts/informe_2.py
+++ scripts/informe_2.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import numpy as np
-import geopandas as gpd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import tabulate as tabulate
-import unicodedata
-from shapely import wkb
 import matplotlib.pyplot as plt
 import math
 import os
